b7c0e087e3ae75d629655dd276dfbc301df34af7
bearsleeping <96886998+bearsleeping@users.noreply.github.com>
2026-03-23 18:43:05 +0100

    delete

--- supabase.config.js
+++
@@ -1,4 +0,0 @@
-window.__SUPABASE_CONFIG__ = {
-  url: "https://xmoidyumwzwulwysjikg.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhtb2lkeXVtd3p3dWx3eXNqaWtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NDIwNDcsImV4cCI6MjA4ODMxODA0N30.JVTz7vcCuvr_OBgPGZn_-Gkx76ojEKnCpy0WHNVsbj4"
-};
